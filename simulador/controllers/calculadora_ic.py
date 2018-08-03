07ff0de3ea57da1c703dcea91651dd3f0ecbad33
Vitor <vitor251093@gmail.com>
2018-08-03 20:26:42 -0300

    Calculadora não deve quebrar se não houverem amostras

--- simulador/controllers/calculadora_ic.py
+++ simulador/controllers/calculadora_ic.py
@@ -11,6 +11,9 @@ class CalculadoraIC(object):
 
     def intervaloDeConfiancaDeAmostras(self, amostras):
         n = len(amostras)
+        if n == 0:
+            return 0, 0
+
         grausDeLiberdade = (n - 1)
         tc = self.tabelaTStudent(grausDeLiberdade)
 
